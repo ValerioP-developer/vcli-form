d0a7b288a7b9374d39c070329a4ad8bbb982a061
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-02-04 17:23:56 -0600

    Added components A-B

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,27 @@
+    <template>
+            <div>
+                    <h1> Component-AAAA  </h1>
+                    <h1>  {{color}} </h1>
+            </div>
+    </template>
+    
+    <script>
+    export default{
+     name :'ComponentA',
+     data(){
+        return {
+             vartest:'white'
+        }
+     },
+     //you need to pass the property from outside there is no value here
+     props:{
+        color: String
+     }
+    }
+    </script>
+    <!-- with scoped you can only have the style on the related component not globally -->
+    <style scoped>
+    div{
+        background-color: red ;
+    }
+    </style>
---
+++ src/components/ComponentB.vue
@@ -0,0 +1,24 @@
+    <template>
+            <div>
+                    <h1> Component-BBB </h1>
+                    <h1>  {{vartest}} </h1>
+                    <button @click="vartest='yellow'"> Change </button>
+            </div>
+    </template>
+    
+    <script>
+    export default{
+     name :'ComponentA',
+     data(){
+        return {
+             vartest:'white'
+        }
+     }
+    }
+    </script>
+    <!-- with scoped you can only have the style on the related component not globally -->
+    <style scoped>
+    div{
+        background-color: red ;
+    }
+    </style>
